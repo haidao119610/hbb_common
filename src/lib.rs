e50432db5f245c1f8b322405974fb67cc4ab2883
haidao9610 <324009610@qq.com>
2026-05-06 01:19:58 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -493,7 +493,9 @@ pub const VER_TYPE_RUSTDESK_CLIENT: &str = "rustdesk-client";
 pub const VER_TYPE_RUSTDESK_SERVER: &str = "rustdesk-server";
 
 pub fn version_check_request(typ: String) -> (VersionCheckRequest, String) {
-    const URL: &str = "https://api.rustdesk.com/version/latest";
+    let url = option_env!("VERSION_CHECK_URL")
+        .unwrap_or("")
+        .to_string();
 
     use sysinfo::System;
     let system = System::new();
@@ -510,7 +512,7 @@ pub fn version_check_request(typ: String) -> (VersionCheckRequest, String) {
             device_id,
             typ,
         },
-        URL.to_string(),
+        url,
     )
 }
 
